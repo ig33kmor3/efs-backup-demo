083bc16d89a75dcc67868dd0c3aad6a1eee78cb7
ig33kmor3 <31199155+ig33kmor3@users.noreply.github.com>
2021-05-18 15:49:18 -0700

    added custom security group to efs

--- iac/stack.py
+++ iac/stack.py
@@ -18,11 +18,27 @@ class EfsBackupDemoStack(cdk.Stack):
             vpc_id=self.VPC_ID
         )
 
+        # create EFS security group with generous rules
+        file_system_sg = ec2.SecurityGroup(
+            self, 'EfsSecurityGroup',
+            vpc=vpc,
+            allow_all_outbound=True
+        )
+
+        file_system_sg.add_ingress_rule(
+            peer=ec2.Peer.ipv4(vpc.vpc_cidr_block),
+            connection=ec2.Port.tcp(2049),
+            description='efs-dev access'
+        )
+
+        file_system_sg.apply_removal_policy(cdk.RemovalPolicy.DESTROY)
+
         # create EFS for sharing with ec2
-        filesystem = efs.FileSystem(
+        file_system = efs.FileSystem(
             self, 'EfsFileSystem',
             vpc=vpc,
             performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
+            security_group=file_system_sg,
             removal_policy=cdk.RemovalPolicy.DESTROY  # demo purposes only
         )
 
